2fbde2af92c04851a0b65617fda88e75ef407679
Polaki Jaya Krishna <polakijayakrishna@gmail.com>
2025-12-15 15:10:00 +0530

    Initial commit: Spring Security JWT auth project setup

---
+++ src/main/java/com/example/security/SpringSecurityJwtAuthApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtAuthApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtAuthApiApplication.class, args);
+    }
+
+}
